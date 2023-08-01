0fe6280f51062673de98d59e5fbb85a9e865a987
handcraft <97497153+firsthandcraft@users.noreply.github.com>
2023-08-01 19:40:51 +0900

    수코딩 1화

--- module/namechange2/printer.js
+++
@@ -1,8 +0,0 @@
-export const title ="codeprinter";
-// export하면 다른 파일 js 에서도 모듈문법으로 불러지면 사용가능
-function print(value){
-    console.log(value);
-}
-//한꺼번에 내보내기 
-// export {title ,print}
-export {title as printerTitle, print}
