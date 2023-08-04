0c3cffba7761f5ac0c3e066fe6c93b6e70009ae2
handcraft <97497153+firsthandcraft@users.noreply.github.com>
2023-08-04 13:27:48 +0900

    es6로 전환

--- module/ default export/members.js
+++
@@ -1,8 +0,0 @@
-export const title ="codeMembers";
-// export하면 다른 파일 js 에서도 모듈문법으로 불러지면 사용가능
-export const data =[
-    'a','b','c','d','e','f','g',
-    'h','i','j','k','l','m','n'
-]
-//default면 하나의 키워드를 내보낼수 있고 딱 한번만 사용할수 있다. 
-export default 'a';
